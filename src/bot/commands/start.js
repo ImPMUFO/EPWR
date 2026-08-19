a0d0cf54a73b3d98f0e2e0bc69ed89895596150b
PMUFO <abasnejadamir0@gmail.com>
2026-08-19 07:53:17 +0330

    Update start.js

--- src/bot/commands/start.js
+++ src/bot/commands/start.js
@@ -1,6 +1,6 @@
 const { getOrCreatePlayer } = require('../../game/player');
-const { formatGold, reply } = require('../../core/helpers');
-const { mainMenu } = require('../keyboards');
+const { formatGold, reply, cb } = require('../../core/helpers');
+const { buildMainMenu } = require('../keyboards');
 
 module.exports = function registerStart(bot) {
   bot.start(async (ctx) => {
@@ -9,18 +9,18 @@ module.exports = function registerStart(bot) {
       let msg = `⚔️ *EPWR*\n\n`;
       msg += `👑 ${player.commander_name} | ⭐ Lv.${player.level}\n`;
       msg += `💰 ${formatGold(player.gold)} | 💎 ${player.gems}`;
-      await reply(ctx, msg, { parse_mode: 'Markdown', ...mainMenu });
+      await reply(ctx, msg, { parse_mode: 'Markdown', ...buildMainMenu(ctx.from.id) });
     } catch (e) {
       await reply(ctx, '⚠️ خطا: ' + e.message);
     }
   });
 
-  bot.action('mainmenu', async (ctx) => {
+  bot.action(/mainmenu:uid:(\d+)/, async (ctx) => {
     await ctx.answerCbQuery();
     const player = await getOrCreatePlayer(ctx.from);
     let msg = `⚔️ *EPWR*\n\n`;
     msg += `👑 ${player.commander_name} | ⭐ Lv.${player.level}\n`;
     msg += `💰 ${formatGold(player.gold)} | 💎 ${player.gems}`;
-    await ctx.editMessageText(msg, { parse_mode: 'Markdown', ...mainMenu });
+    await ctx.editMessageText(msg, { parse_mode: 'Markdown', ...buildMainMenu(ctx.from.id) });
   });
 };
