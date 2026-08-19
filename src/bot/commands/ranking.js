89370916eaae23fa13480ef235b504f946742244
PMUFO <abasnejadamir0@gmail.com>
2026-08-19 07:55:47 +0330

    Update ranking.js

--- src/bot/commands/ranking.js
+++ src/bot/commands/ranking.js
@@ -1,8 +1,8 @@
 const { getSupabase } = require('../../core/supabase');
-const { formatGold } = require('../../core/helpers');
+const { formatGold, cb } = require('../../core/helpers');
 
 module.exports = function registerRanking(bot) {
-  bot.action('ranking', async (ctx) => {
+  bot.action(/ranking:uid:(\d+)/, async (ctx) => {
     await ctx.answerCbQuery();
     const db = getSupabase();
     const { data: players } = await db.from('players')
@@ -10,15 +10,18 @@ module.exports = function registerRanking(bot) {
       .order('level', { ascending: false })
       .limit(10);
 
-    let msg = `🏆 *رتبه‌بندی EPWR*\n\n`;
+    let msg = `🏆 *رتبه‌بندی*\n\n`;
     const medals = ['🥇', '🥈', '🥉'];
     (players || []).forEach((p, i) => {
       const medal = i < 3 ? medals[i] : `${i + 1}.`;
-      msg += `${medal} *${p.commander_name}* | Lv.${p.level} | 💰${formatGold(p.gold)}\n`;
+      msg += `${medal} ${p.commander_name} | Lv.${p.level} | 💰${formatGold(p.gold)}\n`;
     });
 
     if (!players || players.length === 0) msg += `هنوز بازیکنی نیست!`;
 
-    await ctx.editMessageText(msg, { parse_mode: 'Markdown' });
+    await ctx.editMessageText(msg, {
+      parse_mode: 'Markdown',
+      reply_markup: { inline_keyboard: [[{ text: '🔙 بازگشت', callback_data: cb('mainmenu', ctx.from.id) }]] }
+    });
   });
 };
