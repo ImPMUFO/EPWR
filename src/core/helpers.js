c4033bc44e5fe9c836d7190e8f4ee0956bd3cb56
PMUFO <abasnejadamir0@gmail.com>
2026-08-19 07:26:55 +0330

    Update helpers.js

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -10,4 +10,20 @@ function formatGold(amount) {
   return Number(amount).toLocaleString('en-US');
 }
 
-module.exports = { rarityEmoji, rarityName, formatGold };
+// ریپلای روی پیام کاربر
+function getReplyParams(ctx) {
+  if (ctx.message) {
+    return { message_id: ctx.message.message_id };
+  }
+  if (ctx.callbackQuery && ctx.callbackQuery.message) {
+    return { message_id: ctx.callbackQuery.message.message_id };
+  }
+  return undefined;
+}
+
+async function reply(ctx, text, options = {}) {
+  const reply_parameters = getReplyParams(ctx);
+  return ctx.reply(text, { ...options, reply_parameters });
+}
+
+module.exports = { rarityEmoji, rarityName, formatGold, getReplyParams, reply };
