d3d7a35beaab2d346dfdfc11b6c3a080b6e4ab78
PMUFO <abasnejadamir0@gmail.com>
2026-08-20 08:45:16 +0330

    Update settings.js

--- src/bot/commands/settings.js
+++ src/bot/commands/settings.js
@@ -1,5 +1,5 @@
 const { getOrCreatePlayer } = require('../../game/player');
-const { formatGold, cb } = require('../../core/helpers');
+const { formatGold, smartReply, cb } = require('../../core/helpers');
 
 module.exports = function registerSettings(bot) {
   bot.action(/^settings\|(\d+)$/, async (ctx) => {
@@ -12,7 +12,7 @@ module.exports = function registerSettings(bot) {
   bot.action(/^profile\|(\d+)$/, async (ctx) => {
     await ctx.answerCbQuery();
     const player = await getOrCreatePlayer(ctx.from);
-    let msg = `👑 *پروفایل*\n\n🆔 ${player.telegram_id}\n👤 ${player.commander_name}\n🏰 ${player.realm_name}\n⭐ Lv.${player.level} | ✨ ${player.xp}\n\n💰 ${formatGold(player.gold)} | 💎 ${player.gems}`;
+    let msg = `👑 *پروفایل*\n\n🆔 ${player.telegram_id}\n👤 ${player.commander_name}\n🏰 ${player.realm_name}\n⭐ Lv.${player.level} | ✨ ${player.xp || 0}\n\n💰 ${formatGold(player.gold)} | 💎 ${player.gems}`;
     await ctx.editMessageText(msg, { parse_mode: 'Markdown', reply_markup: { inline_keyboard: [[{ text: '🔙', callback_data: cb('mainmenu', ctx.from.id) }]] } });
   });
 };
